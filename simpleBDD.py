11d93c36880257fff32752bb58a03eba19b6c73b
mou002 <150001523+mou002@users.noreply.github.com>
2026-01-26 19:20:36 +0200

    Refactor simpleBDD class for improved structure

--- simpleBDD.py
+++ simpleBDD.py
@@ -104,3 +104,19 @@ bdd1c = simpleBDD("(x3 and not y3) or (not (x3 xor y3) and (x2 and not y2)) or (
 print(f"1c Variables: {bdd1c.variables}")
 print(f"1c Root Node ID: {bdd1c.root}")
 print(f"1c Nodes Created (var_idx, low, high): {bdd1c.nodes}")
+""" 
+Variables: ['x', 'y', 'z']
+Root Node ID: 3
+Nodes Created (var_idx, low, high): {2: (1, 1, 0), 3: (0, 0, 2)}
+1a Variables: ['a', 'b', 'c', 'd']
+1a Root Node ID: 8
+1a Nodes Created (var_idx, low, high): {2: (3, 0, 1), 3: (3, 1, 0), 4: (1, 2, 3), 5: (2, 1, 2), 6: (2, 1, 3), 7: (1, 5, 6), 8: (0, 4, 7)}
+1b Variables: ['x1', 'x2', 'x3', 'x4', 'x5']
+1b Root Node ID: 10
+1b Nodes Created (var_idx, low, high): {2: (4, 0, 1), 3: (3, 0, 2), 4: (2, 0, 3), 5: (3, 2, 1), 6: (2, 3, 5), 7: (1, 4, 6), 8: (2, 5, 1), 9: (1, 6, 8), 10: 
+(0, 7, 9)}
+1c Variables: ['x1', 'x2', 'x3', 'y1', 'y2', 'y3']
+1c Root Node ID: 15
+1c Nodes Created (var_idx, low, high): {2: (5, 1, 0), 3: (2, 0, 2), 4: (4, 2, 0), 5: (4, 1, 2), 6: (2, 4, 5), 7: (1, 3, 6), 8: (3, 4, 0), 9: (3, 5, 2), 10: 
+(2, 8, 9), 11: (3, 2, 4), 12: (3, 1, 5), 13: (2, 11, 12), 14: (1, 10, 13), 15: (0, 7, 14)}
+"""
